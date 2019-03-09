a35d8d90a6865d3fd292bbfbdb31bf4935c8d2b2
deepmanwani18 <deepmanwani18@gmail.com>
2019-03-10 00:08:45 +0530

    a

--- views/assets/a_files/01c82d7f-2988-4cef-89e0-90e0fcb2be94.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-	if (button) {
-    		var button = document.querySelector(".btn.unhelpful");  
-		button.addEventListener("click", function() {
-  			console.log( 'Do something' );
-		});
-	}
-});
